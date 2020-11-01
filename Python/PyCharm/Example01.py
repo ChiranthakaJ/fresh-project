54f3e51e002bad9da5881b4d051db89897d2cf9d
chiranthakaj <chiransj@gmail.com>
2020-11-02 00:08:30 +0530

    First Commit

---
+++ Python/PyCharm/Example01.py
@@ -0,0 +1,11 @@
+# This is how toprint a simple text.
+# Syntax
+# print("Text to be printed")
+print("This is the first sentence printed from Python...!")
+
+# Print a text with a suffix at the end of the sentence...!
+# Syntax
+# print("Text to be printed", end = 'The end of the sentence as the suffix')
+print("This sentence will end with a ", end = '#')
+
+# 
